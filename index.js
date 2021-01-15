c3e6fbb2c0600ecd5219b19637c8ed9b2d0c77df
github username <github email address>
2021-01-14 22:11:46 -0600

    submitted

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    text = document.querySelector('#text')
+    text.textContent = "This is really cool!"
+  });
+
